444fb178dc242309b8c3109f7172383c2a731061
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-01-03 01:36:41 +0300

    hesaplama değişti

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 //2022 Elektirk Faturası Hesaplama
 
-/*
-aktif dağıtım
-Mesken (Düşük Kademe)	0.7946	0.3295
-Mesken (Yüksek Kademe)	1.3488	0.3295
-*/
+let kwh = prompt('Tükettiğiniz "KWH" miktarını giriniz')
 
-// let kwh = prompt('Tükettiğiniz "KWH" miktarını giriniz')
-let kwh = 200
 
 
 
@@ -27,69 +21,27 @@ function fatura(val) {
     }
 
     dagitim = (val * 0.3295)
-
     tüketim = aktif + dagitim
 
-    enerji = tüketim * 0.007
-    btv = tüketim * 0.03205
+    enerji = aktif * 0.007
+    btv = aktif * 0.04
     kdv = (tüketim + enerji + btv) * 0.18
-    total = tüketim + btv + kdv
 
-    console.log(`Aktif Enerji Bedeli ${(aktif).toFixed(2)} TL`)
-    console.log(`Dağıtım Bedeli ${(dagitim).toFixed(2)} TL`)
-    console.warn(`Toplam Tüketim Bedeli ${(tüketim).toFixed(2)} TL`)
+    total = tüketim + btv + kdv + enerji
 
-    console.log(`BTV ${(btv).toFixed(2)} TL`)
-    console.log(`KDV ${(kdv).toFixed(2)} TL`)
-    console.warn(`Vergiler Toplamı ${Number((enerji+btv+kdv).toFixed(2))} TL`)
+    console.warn(`${kwh} kwh Tüketim İçin Fatura Ayrıntıları`)
 
-    console.warn(`Toplam Fatura ${(total).toFixed(2)} TL`)
+    console.log(`Aktif Enerji Bedeli      ${(aktif).toFixed(2)} TL`)
+    console.log(`Dağıtım Bedeli            ${(dagitim).toFixed(2)} TL`)
+    console.warn(`Toplam Tüketim Bedeli   ${(tüketim).toFixed(2)} TL`)
+
+    console.log(`Enerji Fonu                ${(enerji).toFixed(2)} TL`)
+    console.log(`BTV                        ${(btv).toFixed(2)} TL`)
+    console.log(`KDV                       ${(kdv).toFixed(2)} TL`)
+    console.warn(`Vergiler Toplamı         ${(enerji+btv+kdv).toFixed(2)} TL`)
+
+    console.warn(`Toplam Fatura           ${(total).toFixed(2)} TL`)
 }
 
 
 fatura(kwh)
-
-
-
-
-/*
-let kwh = prompt('Tükettiğiniz "KWH" miktarını giriniz')
-
-let limitDown = 1.37
-let limitUp = 2.06
-
-
-function fatura(val) {
-
-    if (val > 150) {
-
-        tüketim = (150 * limitDown) + ((val - 150) * limitUp)
-
-    } else if (val > 0 && val <= 150) {
-
-        tüketim = val * limitDown
-
-    } else {
-
-        return console.log(`Hatalı Giriş Yaptınız`)
-    }
-
-    enerji = tüketim * 0.00455
-    btv = tüketim * 0.03205
-    kdv = (tüketim+enerji+btv) * 0.1808
-    total = tüketim + btv + kdv
-
-    console.log(`Aktif Enerji Bedeli ${(tüketim*0.641).toFixed(2)} TL`)
-    console.log(`Dağıtım Bedeli ${(tüketim*0.359).toFixed(2)} TL`)
-    console.warn(`Toplam Tüketim Bedeli ${(tüketim).toFixed(2)} TL`)
-
-    console.log(`BTV ${(btv).toFixed(2)} TL`)
-    console.log(`KDV ${(kdv).toFixed(2)} TL`)
-    console.warn(`Vergiler Toplamı ${Number((enerji+btv+kdv).toFixed(2))} TL`)
-
-    console.warn(`Toplam Fatura ${(total).toFixed(2)} TL`)
-}
-
-
-fatura(kwh)
-*/
